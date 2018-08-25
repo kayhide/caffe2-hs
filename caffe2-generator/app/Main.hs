561abebbe9bec0270fad90c0380d175f2d442c58
kayhide <kayhide@gmail.com>
2018-08-25 13:51:21 +0900

    try to add function returning std::vector.

--- caffe2-generator/app/Main.hs
+++ caffe2-generator/app/Main.hs
@@ -1,18 +1,22 @@
-import           Foreign.Hoppy.Generator.Main  (defaultMain)
-import           Foreign.Hoppy.Generator.Spec  (Class, Export (..), Function,
-                                                Interface, Module,
-                                                Purity (Nonpure),
-                                                addReqIncludes, ident, ident1,
-                                                includeLocal, includeStd,
-                                                interface,
-                                                interfaceAddHaskellModuleBase,
-                                                makeClass, makeFn, makeModule,
-                                                mkConstMethod, mkCtor,
-                                                moduleAddExports, moduleModify',
-                                                moduleSetCppPath,
-                                                moduleSetHppPath, toExtName)
-import           Foreign.Hoppy.Generator.Std   (c_string, mod_std)
-import           Foreign.Hoppy.Generator.Types (constT, objT, ptrT, refT)
+import           Foreign.Hoppy.Generator.Main       (defaultMain)
+import           Foreign.Hoppy.Generator.Spec       (Class, Export (..),
+                                                     Function, Interface,
+                                                     Module, Purity (Nonpure),
+                                                     Type (..), addReqIncludes,
+                                                     ident, ident1,
+                                                     includeLocal, includeStd,
+                                                     interface,
+                                                     interfaceAddHaskellModuleBase,
+                                                     makeClass, makeFn,
+                                                     makeModule, mkConstMethod, mkMethod,
+                                                     mkCtor, moduleAddExports,
+                                                     moduleModify',
+                                                     moduleSetCppPath,
+                                                     moduleSetHppPath,
+                                                     reqInclude, toExtName)
+import           Foreign.Hoppy.Generator.Std        (c_string, mod_std)
+import qualified Foreign.Hoppy.Generator.Std.Vector as StdVector
+import           Foreign.Hoppy.Generator.Types      (constT, objT, ptrT, refT)
 
 
 main :: IO ()
@@ -22,9 +26,20 @@ interfaceResult :: Either String Interface
 interfaceResult = do
   iface <- interface "caffe2"
            [ mod_wrapper
-           , mod_std
+           , mod_std'
            ]
   interfaceAddHaskellModuleBase ["Caffe2"] iface
+
+c_StringVectorContents :: StdVector.Contents
+c_StringVectorContents = StdVector.instantiate "StdStringVector" (objT c_string) $ reqInclude $ includeStd "vector"
+
+c_StringVector :: Class
+c_StringVector = StdVector.c_vector c_StringVectorContents
+
+mod_std' :: Module
+mod_std' =
+  moduleModify' mod_std $
+  moduleAddExports $ StdVector.toExports c_StringVectorContents
 
 mod_wrapper :: Module
 mod_wrapper =
@@ -53,5 +68,6 @@ c_Workspace =
   makeClass (ident1 "caffe2" "Workspace") (Just $ toExtName "Workspace")
   []
   [ mkCtor "new" []
+  , mkConstMethod "Blobs" [] (refT $ objT c_StringVector)
   , mkConstMethod "GetBlob" [refT . constT $ objT c_string] (ptrT . constT $ objT c_Blob)
   ]
